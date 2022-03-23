a582b8d632dbe98505bf1e2df26d20c0f6bbb19b
SonarSonic <ollielansdell@hotmail.co.uk>
2022-03-23 20:37:05 +0000

    Simplified Drawing Set registering

--- src/main/java/drawingbot/plugins/DiamineInkPlugin.java
+++ src/main/java/drawingbot/plugins/DiamineInkPlugin.java
@@ -7,12 +7,12 @@ import drawingbot.registry.MasterRegistry;
 
 public class DiamineInkPlugin extends AbstractPenPlugin {
 
-  @Override
+    @Override
     public String getPluginName() {
       return "Diamine Ink";
     }
 
-  @Override
+    @Override
     public void registerPens() {
       registerPenWithCode("1", new DrawingPen("Diamine Ink", "1 Graphite", ImageTools.getARGB(255, 99, 99, 99)));
       registerPenWithCode("2", new DrawingPen("Diamine Ink", "2 Earl Grey", ImageTools.getARGB(255, 104, 104, 114)));
@@ -122,8 +122,8 @@ public class DiamineInkPlugin extends AbstractPenPlugin {
       registerPenWithCode("106", new DrawingPen("Diamine Ink", "106 Quartz Black", ImageTools.getARGB(255, 78, 57, 52)));
     }
 
-  @Override
-    public void registerPenSets() {
-      MasterRegistry.INSTANCE.registerDrawingSet(new DrawingSet("Diamine", "Inks", getDrawingPensFromCodes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 , 101 , 102 , 103 , 104 , 105 , 106)));
+    @Override
+    public void registerPenSets(){
+        MasterRegistry.INSTANCE.registerDrawingSet(new DrawingSet("Diamine", "Inks", getDrawingPensFromCodes(manufacturerCodes.keySet().toArray(new Object[0]))));
     }
 }
