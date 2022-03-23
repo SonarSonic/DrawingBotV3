06ab5ac4a0d749d7b4e986e0f6c4138a8eef1b79
led <led@users.noreply.github.com>
2022-03-23 20:56:03 +0000

    Sakura pen plugin (#53)
    * Sakura Pen Plugin
    
    * Fixed module naming
    
    * Fixed numbering discrepancy
    
    * Simplified Drawing Set registering

--- src/main/java/drawingbot/registry/Register.java
+++ src/main/java/drawingbot/registry/Register.java
@@ -8,6 +8,7 @@ import drawingbot.utils.EnumFilterTypes;
 import drawingbot.drawing.*;
 import drawingbot.geom.shapes.*;
 import drawingbot.plugins.CopicPenPlugin;
+import drawingbot.plugins.SakuraPenPlugin;
 import drawingbot.plugins.DiamineInkPlugin;
 import drawingbot.plugins.SpecialPenPlugin;
 import drawingbot.plugins.StaedtlerPenPlugin;
@@ -84,6 +85,7 @@ public class Register implements IPlugin {
     @Override
     public void registerPlugins(List<IPlugin> newPlugins) {
         newPlugins.add(new CopicPenPlugin());
+        newPlugins.add(new SakuraPenPlugin());
         newPlugins.add(new DiamineInkPlugin());
         newPlugins.add(new StaedtlerPenPlugin());
         newPlugins.add(new SpecialPenPlugin());
