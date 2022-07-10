f63d0e5cc38c5854569a408479cfed94c7be3d43
LED <lindsay@pobox.com>
2022-07-10 14:01:18 +0100

    Updated Bic Cristal colours

--- src/main/java/drawingbot/plugins/BicPensPlugin.java
+++ src/main/java/drawingbot/plugins/BicPensPlugin.java
@@ -14,27 +14,16 @@ public class BicPensPlugin extends AbstractPenPlugin {
 
     @Override
     public void registerPens() {
-      registerPenWithCode("1", new DrawingPen("Cristal Ballpoint",  "Fluorescent Yellow", ImageTools.getARGB(255, 253, 236, 45)));
-      registerPenWithCode("2", new DrawingPen("Cristal Ballpoint",  "Fluorescent Orange", ImageTools.getARGB(255, 237, 98, 34)));
-      registerPenWithCode("3", new DrawingPen("Cristal Ballpoint",  "Light Green", ImageTools.getARGB(255, 166, 199, 50)));
-      registerPenWithCode("4", new DrawingPen("Cristal Ballpoint",  "Pink", ImageTools.getARGB(255, 238, 115, 169)));
-      registerPenWithCode("5", new DrawingPen("Cristal Ballpoint",  "Purple", ImageTools.getARGB(255, 144, 103, 169)));
-      registerPenWithCode("6", new DrawingPen("Cristal Ballpoint",  "Light Blue", ImageTools.getARGB(255, 69, 192, 237)));
-      registerPenWithCode("7", new DrawingPen("Cristal Ballpoint",  "Blue", ImageTools.getARGB(255, 28, 98, 169)));
-      registerPenWithCode("8", new DrawingPen("Cristal Ballpoint",  "Red", ImageTools.getARGB(255, 225, 33, 38)));
-      registerPenWithCode("9", new DrawingPen("Cristal Ballpoint",  "Green", ImageTools.getARGB(255, 24, 151, 72)));
-      registerPenWithCode("10", new DrawingPen("Cristal Ballpoint",  "Black", ImageTools.getARGB(255, 43, 43, 43)));
-
-      registerPenWithCode("11", new DrawingPen("Cristal Intensity",  "Yellow", ImageTools.getARGB(255, 47, 190, 9)));
-      registerPenWithCode("12", new DrawingPen("Cristal Intensity",  "Orange", ImageTools.getARGB(255, 251, 136, 119)));
-      registerPenWithCode("13", new DrawingPen("Cristal Intensity",  "Red", ImageTools.getARGB(255, 220, 32, 32)));
-      registerPenWithCode("14", new DrawingPen("Cristal Intensity",  "Pink", ImageTools.getARGB(255, 235, 175, 201)));
-      registerPenWithCode("15", new DrawingPen("Cristal Intensity",  "Purple", ImageTools.getARGB(255, 81, 51, 129)));
-      registerPenWithCode("16", new DrawingPen("Cristal Intensity",  "Light Blue", ImageTools.getARGB(255, 136, 205, 229)));
-      registerPenWithCode("17", new DrawingPen("Cristal Intensity",  "Blue", ImageTools.getARGB(255, 60, 78, 152)));
-      registerPenWithCode("18", new DrawingPen("Cristal Intensity",  "Light Green", ImageTools.getARGB(255, 133, 185, 38)));
-      registerPenWithCode("19", new DrawingPen("Cristal Intensity",  "Green", ImageTools.getARGB(255, 13, 155, 87)));
-      registerPenWithCode("20", new DrawingPen("Cristal Intensity",  "Black", ImageTools.getARGB(255, 20, 20, 20)));
+      registerPenWithCode("10", new DrawingPen("Cristal Ballpoint",  "Black", ImageTools.getARGB(255, 44, 42, 50)));
+      registerPenWithCode("5", new DrawingPen("Cristal Ballpoint",  "Purple", ImageTools.getARGB(255, 77, 34, 138)));
+      registerPenWithCode("7", new DrawingPen("Cristal Ballpoint",  "Blue", ImageTools.getARGB(255, 30, 21, 137)));
+      registerPenWithCode("8", new DrawingPen("Cristal Ballpoint",  "Red", ImageTools.getARGB(255, 207, 57, 74)));
+      registerPenWithCode("9", new DrawingPen("Cristal Ballpoint",  "Green", ImageTools.getARGB(255, 33, 89, 76)));
+      registerPenWithCode("4", new DrawingPen("Cristal Ballpoint",  "Pink", ImageTools.getARGB(255, 218, 38, 129)));
+      registerPenWithCode("3", new DrawingPen("Cristal Ballpoint",  "Light Green", ImageTools.getARGB(255, 94, 135, 71)));
+      registerPenWithCode("6", new DrawingPen("Cristal Ballpoint",  "Light Blue", ImageTools.getARGB(255, 41, 149, 220)));
+      registerPenWithCode("1", new DrawingPen("Cristal Ballpoint",  "Fluorescent Yellow", ImageTools.getARGB(255, 251, 255, 106)));
+      registerPenWithCode("2", new DrawingPen("Cristal Ballpoint",  "Fluorescent Orange", ImageTools.getARGB(255, 255, 145, 90)));
 
       registerPenWithCode("21", new DrawingPen("Cristal Intensity Pastel",  "Verdigris", ImageTools.getARGB(255, 56, 181, 173)));
       registerPenWithCode("22", new DrawingPen("Cristal Intensity Pastel",  "Mustard", ImageTools.getARGB(255, 237, 187, 80)));
@@ -57,3 +46,4 @@ public class BicPensPlugin extends AbstractPenPlugin {
         MasterRegistry.INSTANCE.registerDrawingSet(new DrawingSet("Bic", "Intensity Fineliner Pastel", getDrawingPensFromCodes(21, 22, 14, 23, 24, 25)));
     }
 }
+
