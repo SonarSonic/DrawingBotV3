591da8dcd323064d4f50a41962c5506953722ef7
Ollie Lansdell <SonarSonic@users.noreply.github.com>
2023-12-31 10:31:09 +0000

    Fixed SimpleLineSampler rounding #116

--- src/main/java/drawingbot/pfm/PFMSketchLinesBasic.java
+++ src/main/java/drawingbot/pfm/PFMSketchLinesBasic.java
@@ -17,7 +17,7 @@ public class PFMSketchLinesBasic extends AbstractSketchPFM {
     public void nextPathFindingResult(PathFindingContext context, IPixelData pixels) {
         boolean shading = enableShading && shadingThreshold < lumProgress;
         float deltaAngle = shading ? shadingDeltaAngle : drawingDeltaAngle;
-        float startAngle = tools.randomFloat(startAngleMin, startAngleMax) + 0.5F;
+        float startAngle = tools.randomFloat(startAngleMin, startAngleMax);
         int nextLineLength = tools.randomInt(minLineLength, maxLineLength);
         float avgDarkness = tools.findDarkestLine(pixels, context.getX(), context.getY(), minLineLength, nextLineLength, unlimitedTests ? -1 : lineTests, startAngle, deltaAngle, shading, context.getDstPosition());
 
